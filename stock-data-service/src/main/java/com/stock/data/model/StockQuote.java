f3ca0b374b6e3066e98ac7fd9f48831e8a94b17d
p_liffengli <p_liffengli@tencent.com>
2026-02-06 19:38:17 +0800

    优化debug日志配置和StockQuote模型

--- stock-data-service/src/main/java/com/stock/data/model/StockQuote.java
+++ stock-data-service/src/main/java/com/stock/data/model/StockQuote.java
@@ -1,11 +1,20 @@
 package com.stock.data.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 /**
  * 股票行情数据模型
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class StockQuote {
 
     /**
@@ -76,239 +85,6 @@ public class StockQuote {
     /**
      * 数据来源
      */
+    @Builder.Default
     private String source = "tushare";
-
-    // Constructors
-    public StockQuote() {
-    }
-
-    public StockQuote(String tsCode, String name, String tradeDate, BigDecimal open, BigDecimal high,
-                      BigDecimal low, BigDecimal close, BigDecimal preClose, BigDecimal change,
-                      BigDecimal pctChg, BigDecimal vol, BigDecimal amount, LocalDateTime updateTime, String source) {
-        this.tsCode = tsCode;
-        this.name = name;
-        this.tradeDate = tradeDate;
-        this.open = open;
-        this.high = high;
-        this.low = low;
-        this.close = close;
-        this.preClose = preClose;
-        this.change = change;
-        this.pctChg = pctChg;
-        this.vol = vol;
-        this.amount = amount;
-        this.updateTime = updateTime;
-        this.source = source;
-    }
-
-    // Builder pattern
-    public static StockQuoteBuilder builder() {
-        return new StockQuoteBuilder();
-    }
-
-    // Getters and Setters
-    public String getTsCode() {
-        return tsCode;
-    }
-
-    public void setTsCode(String tsCode) {
-        this.tsCode = tsCode;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTradeDate() {
-        return tradeDate;
-    }
-
-    public void setTradeDate(String tradeDate) {
-        this.tradeDate = tradeDate;
-    }
-
-    public BigDecimal getOpen() {
-        return open;
-    }
-
-    public void setOpen(BigDecimal open) {
-        this.open = open;
-    }
-
-    public BigDecimal getHigh() {
-        return high;
-    }
-
-    public void setHigh(BigDecimal high) {
-        this.high = high;
-    }
-
-    public BigDecimal getLow() {
-        return low;
-    }
-
-    public void setLow(BigDecimal low) {
-        this.low = low;
-    }
-
-    public BigDecimal getClose() {
-        return close;
-    }
-
-    public void setClose(BigDecimal close) {
-        this.close = close;
-    }
-
-    public BigDecimal getPreClose() {
-        return preClose;
-    }
-
-    public void setPreClose(BigDecimal preClose) {
-        this.preClose = preClose;
-    }
-
-    public BigDecimal getChange() {
-        return change;
-    }
-
-    public void setChange(BigDecimal change) {
-        this.change = change;
-    }
-
-    public BigDecimal getPctChg() {
-        return pctChg;
-    }
-
-    public void setPctChg(BigDecimal pctChg) {
-        this.pctChg = pctChg;
-    }
-
-    public BigDecimal getVol() {
-        return vol;
-    }
-
-    public void setVol(BigDecimal vol) {
-        this.vol = vol;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    // Builder class
-    public static class StockQuoteBuilder {
-        private String tsCode;
-        private String name;
-        private String tradeDate;
-        private BigDecimal open;
-        private BigDecimal high;
-        private BigDecimal low;
-        private BigDecimal close;
-        private BigDecimal preClose;
-        private BigDecimal change;
-        private BigDecimal pctChg;
-        private BigDecimal vol;
-        private BigDecimal amount;
-        private LocalDateTime updateTime;
-        private String source = "tushare";
-
-        public StockQuoteBuilder tsCode(String tsCode) {
-            this.tsCode = tsCode;
-            return this;
-        }
-
-        public StockQuoteBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public StockQuoteBuilder tradeDate(String tradeDate) {
-            this.tradeDate = tradeDate;
-            return this;
-        }
-
-        public StockQuoteBuilder open(BigDecimal open) {
-            this.open = open;
-            return this;
-        }
-
-        public StockQuoteBuilder high(BigDecimal high) {
-            this.high = high;
-            return this;
-        }
-
-        public StockQuoteBuilder low(BigDecimal low) {
-            this.low = low;
-            return this;
-        }
-
-        public StockQuoteBuilder close(BigDecimal close) {
-            this.close = close;
-            return this;
-        }
-
-        public StockQuoteBuilder preClose(BigDecimal preClose) {
-            this.preClose = preClose;
-            return this;
-        }
-
-        public StockQuoteBuilder change(BigDecimal change) {
-            this.change = change;
-            return this;
-        }
-
-        public StockQuoteBuilder pctChg(BigDecimal pctChg) {
-            this.pctChg = pctChg;
-            return this;
-        }
-
-        public StockQuoteBuilder vol(BigDecimal vol) {
-            this.vol = vol;
-            return this;
-        }
-
-        public StockQuoteBuilder amount(BigDecimal amount) {
-            this.amount = amount;
-            return this;
-        }
-
-        public StockQuoteBuilder updateTime(LocalDateTime updateTime) {
-            this.updateTime = updateTime;
-            return this;
-        }
-
-        public StockQuoteBuilder source(String source) {
-            this.source = source;
-            return this;
-        }
-
-        public StockQuote build() {
-            return new StockQuote(tsCode, name, tradeDate, open, high, low, close, preClose,
-                    change, pctChg, vol, amount, updateTime, source);
-        }
-    }
 }
